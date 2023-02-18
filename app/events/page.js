b0a15cd60a0a0a5f8a981efe54dc09304ed345d8
Chiranjeev Vishnoi <somu209e@gmail.com>
2023-02-18 10:41:44 +0000

    initial setup done

---
+++ app/events/page.js
@@ -0,0 +1,22 @@
+import Card from '@/components/card'
+
+async function getData() {
+    const res = await fetch("https://dummyjson.com/products/1");
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data');
+    }
+
+    return res.json();
+}
+
+export default function Home() {
+    // const data = await getData();
+
+    return (
+      <>
+        <Card />
+      </>
+    )
+  }
+  
