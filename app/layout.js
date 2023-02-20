63a00a6326bbdd00e406153155564562f8859058
Pratyush <connect2pratyush@gmail.com>
2023-02-21 00:46:16 +0530

    navbar+eventDashboardPages

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import "@/styles/globals.css"
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" class="dark">
       <head />
       <body>{children}</body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Card from '@/components/card'
+import Navbar from '@/components/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <Card />
+      <Navbar />
     </>
   )
 }
