8b3c189d01e91b90ffdc88710c73f894f765cc32
Anjy Gupta <anjyofficial@gmail.com>
2023-03-05 03:55:39 +0530

    added SSR to user receive invite for innoventure and impetus

--- app/manage/impetus/userReceived/page.js
+++ app/manage/impetus/userReceived/page.js
@@ -1,10 +1,35 @@
 import UserReceivedReq from '@/components/userReceivedReq'
-import React from 'react'
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { getServerSession } from 'next-auth';
 
-function UserReceived() {
-  return (
-   <UserReceivedReq eventName="impetus"/>
+async function addMemberData(session) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER}/api/user/impetus/addMember`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${session.accessTokenBackend}`,
+        'Access-Control-Allow-Origin': '*',
+      },
+    },
+    {
+      cache: "no-store",
+    }
   )
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
 }
 
-export default UserReceived
+export default async function UserReceived() {
+  const eventName = "impetus"
+  const session = await getServerSession(authOptions);
+  const data = await addMemberData(session);
+  const requests = data.requests;
+  return (
+    <UserReceivedReq eventName={eventName} requests={requests} session={session} />
+  )
+}
