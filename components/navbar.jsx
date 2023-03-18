11f5f422db2a3ae68798ad55a9c67f3dfdd93b10
Pratyush <connect2pratyush@gmail.com>
2023-03-18 17:53:56 +0530

    navbar responsive

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -15,6 +15,7 @@ export default function Navbar({ session }) {
     });
   };
 
+  const [respHidden, setRespHidden] = useState(false);
   const [dropdown1, setDropdown1] = useState(false);
   const [dropdown2, setDropdown2] = useState(false);
 
@@ -32,24 +33,24 @@ export default function Navbar({ session }) {
             >
               Esummit
             </Link>
-            <button className="focus:shadow-outline rounded-lg focus:outline-none md:hidden">
+            <button onClick={()=>{setRespHidden(prev=>!prev)}} className="focus:shadow-outline rounded-lg focus:outline-none md:hidden">
               <svg fill="currentColor" viewBox="0 0 20 20" className="h-6 w-6">
-                <path
+                {respHidden && <path
                   x-show="!open"
                   fillRule="evenodd"
                   d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
                   clipRule="evenodd"
-                ></path>
-                <path
+                ></path>}
+                {!respHidden && <path
                   x-show="open"
                   fillRule="evenodd"
                   d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
                   clipRule="evenodd"
-                ></path>
+                ></path>}
               </svg>
             </button>
           </div>
-          <nav className="hidden flex-grow flex-col pb-4 md:flex md:flex-row md:justify-end md:pb-0">
+          <nav className={`${respHidden&&'hidden'} flex-grow flex-col pb-4 md:flex md:flex-row md:justify-end md:pb-0`}>
             <Link
               className="dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 focus:shadow-outline mt-2 rounded-lg bg-gray-200 px-4 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-200 hover:text-gray-900 focus:bg-gray-200 focus:text-gray-900 focus:outline-none md:mt-0"
               href="/"
