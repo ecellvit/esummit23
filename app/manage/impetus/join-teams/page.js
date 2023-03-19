c9bbe3913cfc2bc2fb605a5d60c2418a1c117a95
Naitik <naitikjkapadia@gmail.com>
2023-03-20 04:14:47 +0530

    Join Teams Card Removed on Sent Request-Innoventure Weird

--- app/manage/impetus/join-teams/page.js
+++ app/manage/impetus/join-teams/page.js
@@ -1,59 +1,88 @@
-import { authOptions } from '@/pages/api/auth/[...nextauth]';
-import { getServerSession } from 'next-auth';
-import JoinAllTeams from '@/components/joinAllTeams';
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+import JoinAllTeams from "@/components/joinAllTeams";
 
 async function getUserData(session) {
-    const res = await fetch(
-        `${process.env.NEXT_PUBLIC_SERVER}/api/user`,
-        {
-            method: "GET",
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${session.accessTokenBackend}`,
-                'Access-Control-Allow-Origin': '*',
-            },
-        },
-        {
-            cache: "no-store",
-        }
-    );
-    if (!res.ok) {
-        throw new Error("Failed to fetch data");
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER}/api/user`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${session.accessTokenBackend}`,
+        "Access-Control-Allow-Origin": "*",
+      },
+    },
+    {
+      cache: "no-store",
     }
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
-    return res.json();
+  return res.json();
 }
 
-async function getAllteams(session,page) {
-    // const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER}/api/ehack?page=${prev.page}&limit=${prev.limit}`, {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER}/api/impetus?${page}=1&limit=9`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${session.accessTokenBackend}`,
-            'Access-Control-Allow-Origin': '*',
-        },
-
-    },
-        {
-            cache: "no-store",
-        }
-    );
-    if (!res.ok) {
-        throw new Error("Failed to fetch data");
+async function requestSentData(session) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER}/api/user/impetus/requests`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${session.accessTokenBackend}`,
+        "Access-Control-Allow-Origin": "*",
+      },
+      cache: "no-store",
     }
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
-    return res.json();
+  return res.json();
+}
+
+async function getAllteams(session, page) {
+  // const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER}/api/ehack?page=${prev.page}&limit=${prev.limit}`, {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER}/api/impetus?${page}=1&limit=9`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${session.accessTokenBackend}`,
+        "Access-Control-Allow-Origin": "*",
+      },
+    },
+    {
+      cache: "no-store",
+    }
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
 }
 
 export default async function JoinTeams() {
-    const eventName = "impetus"
-    const session = await getServerSession(authOptions);
-    const userData = await getUserData(session);
-    // console.log("userID",userData?.user)
-    const data = await getAllteams(session,1);
-    //   const requests = data.requests;
-    return (
-       <JoinAllTeams session={session} userData={userData} eventName={eventName}/>
-    )
+  const eventName = "impetus";
+  const session = await getServerSession(authOptions);
+  const userData = await getUserData(session);
+  const sentData = await requestSentData(session);
+  console.log(sentData.requests);
+  // console.log("userID",userData?.user)
+  const data = await getAllteams(session, 1);
+  console.log(data.paginatedResult.results);
+  //   const requests = data.requests;
+  return (
+    <JoinAllTeams
+      session={session}
+      userData={userData}
+      eventName={eventName}
+      sentData={sentData.requests}
+    />
+  );
 }
