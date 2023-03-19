a4bb1fac75bcd5655798834f1845e7246b325a2a
Anjy Gupta <anjyofficial@gmail.com>
2023-03-19 23:56:14 +0530

    fixed bugs

--- components/leaderDashboard.js
+++ components/leaderDashboard.js
@@ -44,7 +44,7 @@ export default function LeaderDashboard({ userData, eventName, session }) {
           return;
         }
         refreshData(router, path);
-        toast("Team deleted Successfully");
+        // toast("Team deleted Successfully");
       });
   }
 
