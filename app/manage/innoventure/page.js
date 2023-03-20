7eae6f470e80f5eeeb8644f1071bcda00464e8af
Anjy Gupta <anjyofficial@gmail.com>
2023-03-20 05:46:59 +0530

    fixed innoventure bugs

--- app/manage/innoventure/page.js
+++ app/manage/innoventure/page.js
@@ -2,8 +2,7 @@ import Dashboard from "@/components/dashboard";
 import NotyNav from "@/components/notyNav";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth";
-import hasDetailsFilled from "@/app/utils/hasDetailsFIiled";
-import NotFilledDetails from "@/app/utils/NotFilledDetails";
+
 async function getUserData(session) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER}/api/user`, {
     method: "GET",
@@ -24,25 +23,24 @@ async function getUserData(session) {
 export default async function ManagePage() {
   const eventName = "innoventure";
   const session = await getServerSession(authOptions);
-  const hasDetailsFIiled = session && (await hasDetailsFilled(session));
   const data = await getUserData(session);
   const userData = data.user[eventName + "TeamId"];
   const userRole = data.user[eventName + "TeamRole"];
-    let hasTeam = false;
-    if (userData) {
-      hasTeam = true;
 
-    return (
-      <>
-        <NotyNav eventName={eventName} />
-        <Dashboard
-          eventName={eventName}
-          session={session}
-          hasTeam={hasTeam}
-          userData={userData}
-          userRole={userRole}
-        />
-      </>
-    );
-  } 
+  let hasTeam = false;
+  if (userData) {
+    hasTeam = true;
+  }
+  return (
+    <>
+      <NotyNav eventName={eventName} />
+      <Dashboard
+        eventName={eventName}
+        session={session}
+        hasTeam={hasTeam}
+        userData={userData}
+        userRole={userRole}
+      />
+    </>
+  );
 }
