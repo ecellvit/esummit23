9d1fc45db30c8851719ae39c8d64435871acba4a
Palanikannan1437 <palanikannan.m2020@vitstudent.ac.in>
2023-03-21 13:01:17 +0530

    removed sp char

--- app/Landing/Header.jsx
+++ app/Landing/Header.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Header() {
   return (
     <div className="header">
-      <p className="header_para">E-Cell Vit Presents</p>
-      <h1 className="main_h1">E-SUMMIT’23</h1>
+      <p className="header_para">E-Cell VIT Presents</p>
+      <h1 className="main_h1">{`E-SUMMIT'23`}</h1>
       <p className="para2">
         The 4th edition of the biggest business fest in South India
       </p>
