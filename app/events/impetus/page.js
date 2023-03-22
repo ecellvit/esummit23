3dda730410f7b46edde0fefbf4fd0230833020a2
Chiranjeev-droid <somu209e@gmail.com>
2023-03-22 23:43:28 +0530

    prize pool added

--- app/events/impetus/page.js
+++ app/events/impetus/page.js
@@ -50,20 +50,20 @@ export default async function Home() {
             {`A pitch-deck competition that would involve crafting unique startup ideas and business models to impress a jury of experienced professionals. With numerous startups flourishing in India, this event seeks to nurture the growing spirit of entrepreneurship within young minds. Making your team, investors and users believe in your vision goes a long way in building a successful product with a strong foundation. From getting funded to deploying the first iteration of your product to the public, there’s only one road that’s taken; Pitching`}
             <br />
           </p>
-          {/* <div className="evet_price_wrap">
+          <div className="evet_price_wrap">
             <div className="price_wrap">
-              <p className="para_med_event">1st</p>
-              <p className="para_bold_event">Coming Soon</p>
+              <p className="para_bold_event">Prize Pool - Rs 25,000</p>
+              {/* <p className="para_bold_event">Coming Soon</p> */}
             </div>
-            <div className="price_wrap">
+            {/* <div className="price_wrap">
               <p className="para_med_event">2nd</p>
               <p className="para_bold_event">Coming Soon</p>
             </div>
             <div className="price_wrap">
               <p className="para_med_event">3rd</p>
               <p className="para_bold_event">Coming Soon</p>
-            </div>
-          </div> */}
+            </div> */}
+          </div>
           <RegButton check={check} userArray={userArray} />
         </div>
       </div>
