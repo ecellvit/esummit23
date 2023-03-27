0aa63f3907c335951bcc9c5c0f7a081cc4017cd0
Anjy Gupta <anjyofficial@gmail.com>
2023-03-28 00:29:15 +0530

    fixed bugs

--- components/addMember.js
+++ components/addMember.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-
+import { toast } from "react-toastify";
 import AddMemberCard from "./addMemberCard";
 import { useRouter } from "next/navigation";
 import styles from "../styles/joinTeams.module.css";
